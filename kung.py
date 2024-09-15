5dfa263074167f2bdc34b428ecb8c6b7182cccb1
IsakEngman <isak.engman@elev.ga.ntig.se>
2024-09-15 03:21:58 +0200

    fyfan

--- kung.py
+++ kung.py
@@ -1,21 +1,35 @@
 from random import randint
 
-print("tärningskastspelet")
+player_one_score = 0
+player_two_score = 0
+round = 0
+run = True
 
-player_one = randint(1,6)
+while run:
+    round += 1
+    player_one_roll = randint(1, 6)
+    player_two_roll = randint(1, 6)
 
-player_two = randint(1,6)
+    if player_one_roll > player_two_roll:
+        print("spelare 1 vann")
+        player_one_score += 1
+    elif player_two_roll > player_one_roll:
+        print("spelare 2 vann")
+        player_two_score += 1
+    else:
+        print("Det blev oavgjort")
 
-if player_one > player_two:
-    print("1")
-    player_one_score = int("1")
-elif player_two > player_one:
-    print("2")
-    player_two_score = int("1")
+    if player_one_score >= 2:
+        print("spelare 1 var först att vinna 2 av 3 rundor")
+        run = False
+    elif player_two_score >= 2:
+        print("spelare 2 var först att vinna 2 av 3 rundor")
+        run = False
 
-#totalscore = (player_two_score + player_one_score) 
-#print (totalscore)
-#while totalscore < 3
+    if run is False:
+        rerun = input("vill du köra igen ja/nej?")
+        if rerun == "ja" or rerun == "":
+            run = True
 
 
-    
+
